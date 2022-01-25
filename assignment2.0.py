3aeeb245988030d7f1cb2e2549029701cde5e6aa
Ramsa Ansari <97399728+RamsaAnsari28@users.noreply.github.com>
2022-01-25 12:50:35 +0530

    Add files via upload

---
+++ assignment2.0.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 23 13:23:04 2022
+
+@author: Khalid Ayub Ansari
+"""
+
+#methods of dictionary
+flower={"rose,lily,lotus,tulip"}
+flower.clear()
+
+x= flower.copy()
+
+x=('red1','red2','red3')
+y=0
+thisdict=dict.fromkeys(x,y)
+
+fashion={'brand':'nykaa','model':'lipstick'}
+x=fashion.get("brand")
+
+x=fashion.items()
+
+x=fashion.keys()
+
+x=fashion.pop("model")
+
+x=fashion.popitem()
+
+x=fashion.setdefault('color','pink')
+
+fashion={"year":"2021","month":"january"}
+y=fashion.values()
+
+#create a set
+
+thisset={"rahul,raj,shyam,rohan"}
+print(thisset)
+
+#most frequent character 
+c=[2,4,6,1,3,8,7]
+c.sort(reverse=True)
+print(c)
+
+#filename extension
+
+filename= input("enter file name :")
+exten=filename.split(".")
+print("the extension of file:",exten[-1])
+
+
+
+#calculator using if conditions and functions
+
+print("1 Add")
+print("2 sub")
+
+choice = input("enter your choice: ")
+
+num1= float(input("enter number 1: "))
+num2= float(input("enter number 2:"))
+
+if choice =="1":
+    print(num1,"+", num2,"=",(num1+num2))
+
+
+
+
+
